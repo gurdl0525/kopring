f07ef27f17295dd7f344700cd535a50c4fe54f13
이하성 <tedsoftj1123@gmail.com>
2022-09-05 23:03:24 +0900

    📌 POST ::: DB 연결 및 Entity구현

---
+++ src/main/kotlin/com/example/kopring/entity/Member.kt
@@ -0,0 +1,30 @@
+package com.example.kopring.entity
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Member(
+        name: String,
+        age: Int,
+        password: String
+
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null
+
+    @Column(name = "member_name", length = 5, nullable = false)
+    var name = name
+
+    @Column(name = "member_age", nullable = false)
+    var age = age
+
+    @Column(name = "member_password", length = 60, nullable = false)
+    var password = password
+
+}
